7ce4667474595782b4771c3a04ed89b0e6d7ea43
Alfonso Catanzaro <a.catanzaro@systemretail.it>
2021-03-05 13:59:51 +0100

    first commit

---
+++ OsDataCrypter/IOsDataCipher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OsDataCipher
+{
+    public interface IOsDataCipher
+    {
+        string Encrypt(string data);
+        string Decrypt(string data);
+        bool TryDecrypt(string encrypted, out string decrypted);
+    }
+}
