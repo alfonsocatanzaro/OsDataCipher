a87a44814076a15793ed2d037ab44ff001d05d57
Alfonso Catanzaro <alfonso.catanzaro@gmail.com>
2021-03-29 21:34:45 +0200

    added xml comments

--- OsDataCrypter/IOsDataCipher.cs
+++ OsDataCrypter/IOsDataCipher.cs
@@ -5,10 +5,31 @@ using System.Text;
 
 namespace OsDataCipher
 {
+    /// <summary>
+    /// IOsDataCippher inteface
+    /// </summary>
     public interface IOsDataCipher
     {
+        /// <summary>
+        /// Encrypt a string
+        /// </summary>
+        /// <param name="data">String to encrypt</param>
+        /// <returns>Encrypted string</returns>
         string Encrypt(string data);
+
+        /// <summary>
+        /// Decrypt a crypted string
+        /// </summary>
+        /// <param name="data">String to decrypt</param>
+        /// <returns>Decrypted string</returns>
         string Decrypt(string data);
+
+        /// <summary>
+        /// Try to decrypt a string
+        /// </summary>
+        /// <param name="encrypted">Encrypted string to try for decrypt</param>
+        /// <param name="decrypted">Decrypted strings</param>
+        /// <returns>True for decrypt successfull</returns>
         bool TryDecrypt(string encrypted, out string decrypted);
     }
 }
--- OsDataCrypter/OsDataCipher.cs
+++ OsDataCrypter/OsDataCipher.cs
@@ -6,19 +6,50 @@ using System.Text;
 
 namespace OsDataCipher
 {
+    /// <summary>
+    /// OsDataCipher provide basic and optimized functionality
+    /// for encrypt and decrypt string using a custom-lenght passphrase
+    /// 
+    /// </summary>
     public class OsDataCipher : IOsDataCipher
     {
         private readonly byte[] key;
+
+        //TODO: Create IV from key... 
         private readonly byte[] iv = new byte[] {
             32, 182, 48, 165, 169, 9, 156, 188,
             74, 177, 236, 211, 100, 41, 81, 7
         };
 
+
+        /// <summary>
+        /// Create a <see cref="OsDataCipher"/> class instance
+        /// </summary>
+        /// <param name="password">A password to use for encrypt and decrypt operations</param>
         public OsDataCipher(string password)
         {
+            //Convert password string to byte array
             byte[] passPhraseAsByteArray = Encoding.UTF8.GetBytes(password);
+            // create hash (16 bit) of the byte array and using it for AES algoritm
             key = SHA256.Create().ComputeHash(passPhraseAsByteArray);
         }
+
+        /// <summary>
+        /// Encrypt a string
+        /// </summary>
+        /// <param name="data">String to encrypt</param>
+        /// <returns>Base64 encrypted string</returns>
+        public string Encrypt(string data)
+        {
+            return Convert.ToBase64String(
+                Encrypt(Encoding.UTF8.GetBytes(data)));
+        }
+
+        /// <summary>
+        /// Decrypt a crypted string
+        /// </summary>
+        /// <param name="data">Base64 string to decrypt</param>
+        /// <returns>Decrypted string</returns>
         public string Decrypt(string data)
         {
             return Encoding.UTF8.GetString(
@@ -26,12 +57,12 @@ namespace OsDataCipher
                        Convert.FromBase64String(data)));
         }
 
-        public string Encrypt(string data)
-        {
-            return Convert.ToBase64String(
-                Encrypt(Encoding.UTF8.GetBytes(data)));
-        }
-
+        /// <summary>
+        /// Try to decrypt a string
+        /// </summary>
+        /// <param name="encrypted">Base64 encrypted string to try for decrypt</param>
+        /// <param name="decrypted">Decrypted strings</param>
+        /// <returns>True for decrypt successfull</returns>
         public bool TryDecrypt(string encrypted, out string decrypted)
         {
             try
